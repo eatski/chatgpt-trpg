13445d13c30ca813ccbd6d2c669ffb126663711a
hagaitski <i.haga629@gmail.com>
2023-04-23 19:57:15 +0900

    submit message feature

--- src/features/session/session.tsx
+++ src/features/session/session.tsx
@@ -1,9 +1,8 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Scenario } from "@/models/types";
-import { addDoc } from "@firebase/firestore";
-import { getCollectionRef } from "@/lib/firestore";
 import { useSubmitChatGpt } from "./useSubmitChatGpt";
 import { Feed } from "../feed";
+import { SubmitMessage } from "../submitMessage";
 
 type Props = {
   roomId: string;
@@ -12,41 +11,13 @@ type Props = {
 
 const Session: React.FC<Props> = ({ roomId, scenario }) => {
   const userId = "test";
-  const chatCollection = useMemo(() => getCollectionRef(`rooms/${roomId}/chat`), [roomId]);
-
   useSubmitChatGpt({ roomId, scenario });
-  const [input, setInput] = React.useState<string>("");
   return (
     <section>
       <h2>{scenario.title}</h2>
       <p>{scenario.description}</p>
       <Feed roomId={roomId} />
-      <div>
-        <input
-          value={input}
-          onChange={async (e) => {
-            setInput(e.target.value);
-          }}
-        ></input>
-        {
-          <button
-            disabled={!input}
-            onClick={() => {
-              addDoc(chatCollection, {
-                user: {
-                  userId,
-                  message: input,
-                },
-                createdAt: new Date().getTime(),
-              }).then(() => {
-                setInput("");
-              });
-            }}
-          >
-            送信
-          </button>
-        }
-      </div>
+      <SubmitMessage userId={userId} roomId={roomId} />
     </section>
   );
 };
---
+++ src/features/submitMessage/SubmitMessageView/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+
+export type Props = {
+  onSubmit: (message: string) => void;
+};
+
+export const SubmitMessageView: React.FC<Props> = ({ onSubmit: onClick }) => {
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (message.trim()) {
+      onClick(message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <form className={styles.submitMessageForm} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className={styles.messageInput}
+        value={message}
+        onChange={handleChange}
+        placeholder="Type your message here..."
+      />
+      <button type="submit" className={styles.submitButton}>
+        Send
+      </button>
+    </form>
+  );
+};
