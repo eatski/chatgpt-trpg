565e2d57263315197acb4465217cf33ea19a6849
hagaitski <i.haga629@gmail.com>
2023-04-27 03:43:31 +0900

    出力をファジーに

--- src/adapters/chatGptAssistantResponse.ts
+++ src/adapters/chatGptAssistantResponse.ts
@@ -6,7 +6,8 @@ export const getChatGptJsonResponse = async <T>(messages: ChatCompletionRequestM
   const res = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages,
-    temperature: 0.1,
+    temperature: 0.3,
+    frequency_penalty: 0.4
   });
   const content = res.data.choices[0].message?.content;
   if (!content) {
